5a7e5406c2eda8c1cfd96b08cc98d9d17b2a6dd8
Mohitjnn <jnmohit29@gmail.com>
2024-08-16 05:59:06 +0530

    artisan hire form made

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Redirect } from 'expo-router';
 
 export default function AppLayout() {
-  return <Redirect href="/artisan/workshop/host" />;
-  // return <Redirect href="/artisan/portal/rentalMachine/create" />;
+  // return <Redirect href="/artisan/workshop/host" />;
+  return <Redirect href="/artisan/portal/job/mohit/apply" />;
 }
